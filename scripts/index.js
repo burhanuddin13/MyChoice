4a66e65b118aadaaeaa4baf177f6d024bd87bcc3
Burhanuddin Murtuza Fatehi <burhanuddin.fatehi@gmail.com>
2023-10-13 22:13:40 +0530

    reading time added

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ const cardHeader = document.querySelectorAll("card-header");
 const cardContent = document.querySelectorAll("card-content");
 const expand = document.getElementsByClassName("expand");
 const collapse = document.getElementsByClassName("collapse");
+const readTime = document.querySelectorAll("read-time");
 
 cardHeader.forEach((header, index) => {
   header.addEventListener("click", () => {
@@ -10,11 +11,13 @@ cardHeader.forEach((header, index) => {
       cardContent[index].style.display = "none";
       expand[index].style.display = "block";
       collapse[index].style.display = "none";
+      readTime[index].innerText = "";
     } else {
       cardContent[index].style.display = "block";
       header.style.borderRadius = "30px 30px 0px 0px";
       expand[index].style.display = "none";
       collapse[index].style.display = "block";
+      readTime[index].innerText = getReadTime(cardContent[index].innerText);
     }
     if (cardContent[index].style.maxHeight) {
       cardContent[index].style.maxHeight = null;
@@ -39,7 +42,8 @@ navLinks.forEach((navLink, index) => {
     expand[index].style.display = "none";
     collapse[index].style.display = "block";
     cardContent[index].style.maxHeight = cardContent[index].scrollHeight + "px";
-    navPanel.style.display = "none"
+    navPanel.style.display = "none";
+    readTime[index].innerText = getReadTime(cardContent[index].innerText);
   });
 });
 
@@ -48,3 +52,13 @@ document.addEventListener("click", (event) => {
     navPanel.style.display = "none";
   }
 })
+
+const getReadTime = (text) => {
+  const wordsPerMinute = 200;
+  const numberOfWords = text.split(/\s/g).length;
+  const readTime = numberOfWords / wordsPerMinute;
+  const minutes = Math.floor(readTime);
+  const secondsDecimal = readTime - minutes;
+  const seconds = Math.round(secondsDecimal * 60);
+  return `${numberOfWords} words | Reading Time: ${minutes} min ${seconds} sec`;
+}
