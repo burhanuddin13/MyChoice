f346822e28cd70542e8adfffb3180925fbd7e1ab
Burhanuddin Murtuza Fatehi <burhanuddin.fatehi@gmail.com>
2023-10-16 23:24:39 +0530

    text to speech

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,9 @@ const cardContent = document.querySelectorAll("card-content");
 const expand = document.getElementsByClassName("expand");
 const collapse = document.getElementsByClassName("collapse");
 const readTime = document.querySelectorAll("read-time");
+const textToSpeech = document.querySelectorAll(".text-to-speech");
+
+var utterance = new SpeechSynthesisUtterance();
 
 cardHeader.forEach((header, index) => {
   header.addEventListener("click", () => {
@@ -62,3 +65,82 @@ const getReadTime = (text) => {
   const seconds = Math.round(secondsDecimal * 60);
   return `Number of Words: ${numberOfWords} words \n Reading Time: ${minutes} min ${seconds} sec`;
 }
+
+const textToSpeechButtons = document.querySelectorAll(".tts-button");
+const speechControls = document.querySelectorAll(".speech-control");
+const volumeSliders = document.querySelectorAll(".volume-slider");
+const speedSliders = document.querySelectorAll(".speed-slider");
+const pauseButtons = document.querySelectorAll(".pause-button");
+const stopButtons = document.querySelectorAll(".stop-button");
+const playButtons = document.querySelectorAll(".play-button");
+const resumeButtons = document.querySelectorAll(".resume-button");
+
+textToSpeechButtons.forEach((textToSpeechButton, index) => {
+  textToSpeechButton.addEventListener("click", () => {
+    const text = cardContent[index].innerText.slice(10);
+    utterance = new SpeechSynthesisUtterance(text);
+    speechControls[index].style.display = "flex";
+    volumeSliders[index].value = utterance.volume;
+    speedSliders[index].value = utterance.rate;
+  });
+});
+
+pauseButtons.forEach((pauseButton, index) => {
+  pauseButton.addEventListener("click", () => {
+    speechSynthesis.pause();
+  });
+});
+
+resumeButtons.forEach((resumeButton, index) => {
+  resumeButton.addEventListener("click", () => {
+    speechSynthesis.resume();
+  });
+});
+
+stopButtons.forEach((stopButton, index) => {
+  stopButton.addEventListener("click", () => {
+    speechSynthesis.cancel();
+    playButtons[index].style.display = "flex";
+    pauseButtons[index].style.display = "none";
+    volumeSliders[index].disabled = false;
+    speedSliders[index].disabled = false;
+  });
+});
+
+volumeSliders.forEach((volumeSlider, index) => {
+  volumeSlider.addEventListener("change", () => {
+    utterance.volume = volumeSlider.value;
+    console.log(utterance.volume);
+  });
+});
+
+speedSliders.forEach((speedSlider, index) => {
+  speedSlider.addEventListener("change", () => {
+    utterance.rate = speedSlider.value;
+    console.log(utterance.rate);
+  });
+});
+
+playButtons.forEach((playButton, index) => {
+  playButton.addEventListener("click", () => {
+    console.log(index);
+    speechSynthesis.cancel();
+    utterance.volume = volumeSliders[index].value;
+    utterance.rate = speedSliders[index].value;
+    speechSynthesis.speak(utterance);
+    playButtons[index].style.display = "none";
+    pauseButtons[index].style.display = "flex";
+    volumeSliders[index].disabled = true;
+    speedSliders[index].disabled = true;
+  });
+});
+
+speechSynthesis.addEventListener("end", () => {
+  playButtons.forEach((playButton, index) => {
+    playButton.style.display = "flex";
+    pauseButtons[index].style.display = "none";
+    resumeButtons[index].style.display = "none";
+    volumeSliders[index].disabled = false;
+    speedSliders[index].disabled = false;
+  });
+});
